8d916427858d47c33f20230a2ed0087841ecf202
Valay Desai <chako.pakodi@gmail.com>
2020-08-25 15:52:50 -0400

    feat: reducers and required packages

---
+++ src/reducers/users.js
@@ -0,0 +1,13 @@
+import { RECEIVE_USERS } from '../actions/users';
+
+export function receiveUsers(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users,
+      };
+    default:
+      return state;
+  }
+}
