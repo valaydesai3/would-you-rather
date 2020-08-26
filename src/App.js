c1cac102a6cf947786b346f1ed90c2f13149f256
Valay Desai <chako.pakodi@gmail.com>
2020-08-26 08:17:37 -0400

    feat: react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleInitialData } from './actions/shared';
+import Login from './components/Login';
+import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +11,24 @@ class App extends Component {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>Would you rather?</div>;
+    const { authedUser } = this.props;
+
+    return (
+      <BrowserRouter>
+        <div className="app">
+          {authedUser === null ? (
+            <Route render={() => <Login />} />
+          ) : (
+            <Route exact path="/" component={Home} />
+          )}
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return { authedUser };
+}
+
+export default connect(mapStateToProps)(App);
