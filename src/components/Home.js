de712cc6437ffa6d9cf1ecea35c638161c3d2b71
Valay Desai <chako.pakodi@gmail.com>
2020-09-08 16:19:42 -0400

    refactor: Home functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes, { string } from 'prop-types';
 import Tabs from '../components/Tabs';
 import Question from './Question';
 
-class Home extends Component {
-  static propTypes = {
-    answeredQuestions: PropTypes.arrayOf(string).isRequired,
-    unansweredQuestions: PropTypes.arrayOf(string).isRequired,
-  };
-  render() {
-    const { answeredQuestions, unansweredQuestions } = this.props;
-    return (
-      <div>
-        <Tabs>
-          <div label="Unanswered Questions">
-            {unansweredQuestions.length === 0
-              ? `You've answered all the questions.`
-              : unansweredQuestions.map((item) => (
-                  <Question key={item} id={item} />
-                ))}
-          </div>
-          <div label="Answered Questions">
-            {answeredQuestions.map((item) => (
-              <Question key={item} id={item} />
-            ))}
-          </div>
-        </Tabs>
-      </div>
-    );
-  }
-}
+const Home = (props) => {
+  const { answeredQuestions, unansweredQuestions } = props;
+  return (
+    <div>
+      <Tabs>
+        <div label="Unanswered Questions">
+          {unansweredQuestions.length === 0
+            ? `You've answered all the questions.`
+            : unansweredQuestions.map((item) => (
+                <Question key={item} id={item} />
+              ))}
+        </div>
+        <div label="Answered Questions">
+          {answeredQuestions.map((item) => (
+            <Question key={item} id={item} />
+          ))}
+        </div>
+      </Tabs>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  answeredQuestions: PropTypes.arrayOf(string).isRequired,
+  unansweredQuestions: PropTypes.arrayOf(string).isRequired,
+};
 
 function mapStateToProps({ authedUser, questions, users }) {
   const answeredQuestions = Object.keys(users[authedUser].answers).sort(
