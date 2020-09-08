809bc9fd341f9af6367dbf63873ee1ff777df9f2
Valay Desai <chako.pakodi@gmail.com>
2020-09-08 17:51:09 -0400

    refactor: Question functional component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class Question extends Component {
-  static propTypes = {
-    question: PropTypes.object.isRequired,
-    author: PropTypes.object.isRequired,
+const Question = (props) => {
+  const { question, author } = props;
+
+  const viewPoll = (qid) => {
+    props.history.push(`/questions/${qid}`);
   };
 
-  viewPoll = (e, qid) => {
-    this.props.history.push(`/questions/${qid}`);
-  };
-
-  render() {
-    const { question, author } = this.props;
-    return (
-      <div className="card">
-        <div className="card-header">{author.name} asks:</div>
-        <div className="card-img">
-          <img src={author.avatarURL} alt="avatar" />
-        </div>
-        <div className="card-content">
-          <p className="card-content-label">Would you rather...</p>
-          <p>
-            {`...`}
-            {question.optionOne.text.substr(0, 10)}
-            {`...`}
-          </p>
-          <div className="card-content-action">
-            <button onClick={(e) => this.viewPoll(e, question.id)}>
-              View Poll
-            </button>
-          </div>
+  return (
+    <div className="card">
+      <div className="card-header">{author.name} asks:</div>
+      <div className="card-img">
+        <img src={author.avatarURL} alt="avatar" />
+      </div>
+      <div className="card-content">
+        <p className="card-content-label">Would you rather...</p>
+        <p>
+          {`...`}
+          {question.optionOne.text.substr(0, 10)}
+          {`...`}
+        </p>
+        <div className="card-content-action">
+          <button onClick={() => viewPoll(question.id)}>View Poll</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Question.propTypes = {
+  question: PropTypes.object.isRequired,
+  author: PropTypes.object.isRequired,
+};
 
 function mapStateToProps({ questions, users }, { id }) {
   return {
