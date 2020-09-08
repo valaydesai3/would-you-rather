f343a3dbf48d416bfce9498e077d58a14ce7d7db
Valay Desai <chako.pakodi@gmail.com>
2020-09-08 16:15:59 -0400

    refactor: SignIn functional component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,70 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes, { object } from 'prop-types';
 import { setAuthedUser } from '../actions/authedUser';
 
-class SignIn extends Component {
-  static propTypes = {
-    users: PropTypes.arrayOf(object).isRequired,
-    setAuthedUser: PropTypes.func.isRequired,
+const SignIn = (props) => {
+  const [user, setUserSelected] = useState('');
+
+  const userSelected = (e) => {
+    setUserSelected(e.target.value);
   };
 
-  state = {
-    user: '',
-  };
-
-  userSelected = (e) => {
-    this.setState({ user: e.target.value });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { user } = this.state;
-    const { setAuthedUser } = this.props;
-    setAuthedUser(user);
+    props.setAuthedUser(user);
   };
 
-  render() {
-    const { users } = this.props;
-    const { user } = this.state;
-    const items = users.map((user) => (
-      <option key={user.id} value={user.id}>
-        {user.name}
-      </option>
-    ));
-    return (
-      <div className="signin">
-        <div className="signin-header">
-          <p className="welcome">Welcome to the Would You Rather App!</p>
-          <p>Please sign in to continue</p>
-        </div>
-        <div className="signin-image-holder">
-          <img className="signin-image" src="logo512.png" alt="logo" />
-        </div>
-        <h2 className="signin-label">Sign In</h2>
-        <div className="signin-form">
-          <select
-            className="signin-select"
-            onChange={this.userSelected}
-            value={user}
-          >
-            <option value="" defaultValue="" disabled>
-              Select User
-            </option>
-            {items}
-          </select>
-          <button
-            className="signin-btn"
-            disabled={user === ''}
-            onClick={this.handleSubmit}
-          >
-            Sign In
-          </button>
-        </div>
+  const items = props.users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <div className="signin">
+      <div className="signin-header">
+        <p className="welcome">Welcome to the Would You Rather App!</p>
+        <p>Please sign in to continue</p>
       </div>
-    );
-  }
-}
+      <div className="signin-image-holder">
+        <img className="signin-image" src="logo512.png" alt="logo" />
+      </div>
+      <h2 className="signin-label">Sign In</h2>
+      <div className="signin-form">
+        <select className="signin-select" onChange={userSelected} value={user}>
+          <option value="" defaultValue="" disabled>
+            Select User
+          </option>
+          {items}
+        </select>
+        <button
+          className="signin-btn"
+          disabled={user === ''}
+          onClick={handleSubmit}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+SignIn.propTypes = {
+  users: PropTypes.arrayOf(object).isRequired,
+  setAuthedUser: PropTypes.func.isRequired,
+};
 
 function mapStateToProps({ users }) {
   return {
