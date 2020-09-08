5a0607514813d8174defa90568d1ea33e2536bf6
Valay Desai <chako.pakodi@gmail.com>
2020-09-08 19:51:00 -0400

    Updates

---
+++ precache-manifest.59ed5de5fc90cd02cc82e26d6767315a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55b4e46def056badbfaf455c8eebd96",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "72f601eb32d41b2deca0",
+    "url": "/would-you-rather/static/css/main.15b57acf.chunk.css"
+  },
+  {
+    "revision": "31ec5b4bb5cddc2fd586",
+    "url": "/would-you-rather/static/js/2.9b5010b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/would-you-rather/static/js/2.9b5010b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f601eb32d41b2deca0",
+    "url": "/would-you-rather/static/js/main.3f7cc911.chunk.js"
+  },
+  {
+    "revision": "4e275b564a8bb03f6c59",
+    "url": "/would-you-rather/static/js/runtime-main.fb0fae1a.js"
+  }
+]);
